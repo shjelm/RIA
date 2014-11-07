0310379280ec48529e52c01019b9f6a176f85c39
shjelm <shjelm93@gmail.com>
2014-11-07 12:09:07 +0100

    Testar

--- js/main.js
+++ js/main.js
@@ -2,16 +2,15 @@ var Firebase = require("firebase");
 
 var myFirebaseRef = new Firebase("https://ria2014.firebaseio.com/");
 
-myFirebaseRef.set({
-  title: "Hello World!",
+myFirebaseRef.set("testing");
+/*title: "Hello World!",
   author: "Firebase",
   location: {
     city: "San Francisco",
     state: "California",
     zip: 94103
   }
-});
 
 myFirebaseRef.child("location/city").on("value", function(snapshot) {
   alert(snapshot.val());  // Alerts "San Francisco"
-});
+});*/
