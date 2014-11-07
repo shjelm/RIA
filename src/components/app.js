f95be4e27e9bb49ca0b9f1f4416b6862cdbbbadb
shjelm <shjelm93@gmail.com>
2014-11-07 13:06:12 +0100

    .

--- src/components/app.js
+++ src/components/app.js
@@ -14,5 +14,5 @@ var myFirebaseRef = new Firebase("https://ria2014.firebaseio.com/");
 myFirebaseRef.child("location/city").on("value", function(snapshot) {
 	var node = document.getElementById('node-id');
 	var string = '<p>'+snapshot.val()+'</p>';
-	node.innerHTML(string);
+	node.innerHTML = string;
 });
