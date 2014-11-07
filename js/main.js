a3ee3dba6c433ffd5ec11df8e892f41386eeb36b
shjelm <shjelm93@gmail.com>
2014-11-07 11:54:56 +0100

    Read/write

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var Firebase = require("firebase");
+
+var myFirebaseRef = new Firebase("https://ria2014.firebaseio.com/");
+
+myFirebaseRef.set({
+  title: "Hello World!",
+  author: "Firebase",
+  location: {
+    city: "San Francisco",
+    state: "California",
+    zip: 94103
+  }
+});
+
+myFirebaseRef.child("location/city").on("value", function(snapshot) {
+  alert(snapshot.val());  // Alerts "San Francisco"
+});
