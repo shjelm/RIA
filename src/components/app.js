cf304aa49bf5849caca799102271e6c208634dcc
shjelm <shjelm93@gmail.com>
2014-11-19 15:34:31 +0100

    x

--- src/components/app.js
+++ src/components/app.js
@@ -1,32 +1,35 @@
 /** @jsx React.DOM */
 
-var Firebase = require("firebase");
-var myFirebaseRef = new Firebase("https://ria2014.firebaseio.com/");
-
-myFirebaseRef.set({
-  title: "Hello World!",
-  author: "Firebase",
-  location: {
-    city: "San Francisco",
-    state: "California",
-    zip: 94103
-  }
+var APP = Render.createClass({
+	render: function(){
+		return (
+      React.createElement('div', {className: "commentBox"},
+        "Hello, world! I am a CommentBox."
+      )
+    );
+   }
 });
+module.exports = App;
 
-myFirebaseRef.child("location/city").on("value", function(snapshot) {
-	var node = document.getElementById('node-id');
-	var string = '<p>'+snapshot.val()+'</p>';
-	node.innerHTML = string;
-});
 
-// var APP = Render.createClass({
-	// render: function(){
-		// return (
-      // React.createElement('div', {className: "commentBox"},
-        // "Hello, world! I am a CommentBox."
-      // )
-    // );
-   // }
+
+
+// var Firebase = require("firebase");
+// var myFirebaseRef = new Firebase("https://ria2014.firebaseio.com/");
+// 
+// myFirebaseRef.set({
+  // title: "Hello World!",
+  // author: "Firebase",
+  // location: {
+    // city: "San Francisco",
+    // state: "California",
+    // zip: 94103
+  // }
 // });
-
+// 
+// myFirebaseRef.child("location/city").on("value", function(snapshot) {
+	// var node = document.getElementById('node-id');
+	// var string = '<p>'+snapshot.val()+'</p>';
+	// node.innerHTML = string;
+// });
 
