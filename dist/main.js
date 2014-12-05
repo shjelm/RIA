e747049db75d8bfc400729091c1f95fbf455b866
shjelm <shjelm93@gmail.com>
2014-12-05 14:51:49 +0100

    blaha

--- dist/main.js
+++ dist/main.js
@@ -30019,20 +30019,43 @@ var Play = React.createClass({displayName: 'Play',
 	getInitialState: function(){
 		return {questions:{}};
 	},
-	componentWillMount: function() {
+	getButton: function(){
+		if(_.isEmpty(this.state.questions)){
+			return "Load game";
+		}
+		else{
+			return "Start game";
+		}		
+	},
+	runGame: function(){
+		console.log("hej");
+		_.map(this.state.questions,function(q){
+	          		return React.createElement(GuessQuestion, {data: q});
+		        });
+	},
+	handleClick: function() {
+ 	 	if(_.isEmpty(this.state.questions)){
+			this.loadQuestions();
+		}
+		else{
+			this.runGame();	
+		}
+   	},
+	loadQuestions: function() {
 		var me = this;
 		this.ref = new Firebase("https://ria2014.firebaseio.com/");
-		this.ref.child('questions').limitToLast(1).on("value", function(data) {
+		this.ref.child('questions').limitToLast(10).once("value", function(data) {
 			me.setState({'questions':data.val()});
 		});
     },
 	render: function() {
 		 return(
 			React.createElement("div", {id: "game"}, 
-			React.createElement("h2", null, "Let's play! "), 	
+			React.createElement("h2", null, "Let's play! "), 
+				React.createElement("button", {ref: "button", value: this.getButton(), onClick: this.handleClick}, this.getButton()), 
 				_.map(this.state.questions,function(q){
-		          	return React.createElement(GuessQuestion, {data: q});
-		          })
+	          		return React.createElement(GuessQuestion, {data: q});
+		        })
 			)
 		);
 	}
--- src/components/play_game.js
+++ src/components/play_game.js
@@ -10,20 +10,43 @@ var Play = React.createClass({
 	getInitialState: function(){
 		return {questions:{}};
 	},
-	componentWillMount: function() {
+	getButton: function(){
+		if(_.isEmpty(this.state.questions)){
+			return "Load game";
+		}
+		else{
+			return "Start game";
+		}		
+	},
+	runGame: function(){
+		console.log("hej");
+		_.map(this.state.questions,function(q){
+	          		return <GuessQuestion data={q}/>;
+		        });
+	},
+	handleClick: function() {
+ 	 	if(_.isEmpty(this.state.questions)){
+			this.loadQuestions();
+		}
+		else{
+			this.runGame();	
+		}
+   	},
+	loadQuestions: function() {
 		var me = this;
 		this.ref = new Firebase("https://ria2014.firebaseio.com/");
-		this.ref.child('questions').limitToLast(1).on("value", function(data) {
+		this.ref.child('questions').limitToLast(10).once("value", function(data) {
 			me.setState({'questions':data.val()});
 		});
     },
 	render: function() {
 		 return(
 			<div id = "game">
-			<h2>Let's play! </h2>	
+			<h2>Let's play! </h2>
+				<button ref="button" value={this.getButton()} onClick={this.handleClick}>{this.getButton()}</button>
 				{_.map(this.state.questions,function(q){
-		          	return <GuessQuestion data={q}/>;
-		          })}
+	          		return <GuessQuestion data={q}/>;
+		        })}
 			</div>
 		);
 	}
